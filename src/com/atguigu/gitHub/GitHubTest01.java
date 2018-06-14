b0252be020354494973a1e7b96d29a820da1ee22
lcg <lsg13283773980@126.com>
2018-06-14 17:29:34 +0800

    更新2

--- src/com/atguigu/gitHub/GitHubTest01.java
+++ src/com/atguigu/gitHub/GitHubTest01.java
@@ -5,6 +5,7 @@ public class GitHubTest01 {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
 		System.out.println("Hello GitHub 2");
+		System.out.println("Hello GitHub 2");
 	}
 
 }
